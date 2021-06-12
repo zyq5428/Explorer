bd02b8c10a00b1311e2c586417896a6b383fb50e
zyq5428 <594270026@qq.com>
2021-06-13 01:48:37 +0800

    add learning crawler

---
+++ Document/Python学习/Crawler/multiprocess.py
@@ -0,0 +1,23 @@
+from multiprocessing import Pool
+import urllib.request
+import urllib.error
+
+
+def scrape(url):
+    try:
+        urllib.request.urlopen(url)
+        print(f'URL {url} Scraped')
+    except (urllib.error.HTTPError, urllib.error.URLError):
+        print(f'URL {url} not Scraped')
+
+
+if __name__ == '__main__':
+    pool = Pool(processes=3)
+    urls = [
+        'https://www.baidu.com',
+        'http://www.meituan.com/',
+        'http://blog.csdn.net/',
+        'http://xxxyxxx.net'
+    ]
+    pool.map(scrape, urls)
+    pool.close()
